f7b49664f0b96409f5730bf656b6d8c84f75d751
Oscar Caballero <oscarcpozas@gmail.com>
2022-11-01 23:02:44 +0100

    Minor changes on window.rs

--- emu/src/window.rs
+++ emu/src/window.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use minifb::{Scale, Window, WindowOptions};
 
 const WINDOW_WIDTH: usize = 160;
 const WINDOW_HEIGHT: usize = 144;
+const WINDOW_TITLE: &str = "Gameboy emulator (github.com/oscarcpozas/gb_emu)";
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)] // TODO: Review this
 pub enum GameBoyKey {
@@ -18,42 +20,35 @@ pub enum GameBoyKey {
 }
 
 pub struct GUI {
-    pub window: Window,
-    pub vram: Vec<u32>,
-    pub keys_states: HashMap<GameBoyKey, bool>
+    pub vram: Arc<Mutex<Vec<u32>>>,
+    pub keys_states: Arc<Mutex<HashMap<GameBoyKey, bool>>>,
 }
 
 impl GUI {
     pub fn new() -> Self {
+        GUI {
+            keys_states: Arc::new(Mutex::new(GUI::new_key_states())),
+            vram: Arc::new(Mutex::new(vec![0; WINDOW_WIDTH * WINDOW_HEIGHT]))
+        }
+    }
+
+    pub fn run(mut self) {
         let window_options = WindowOptions {
             resize: false,
             scale: Scale::X4,
             ..WindowOptions::default()
         };
-
         let mut window =
-            match Window::new(
-                "Gameboy emulator (github.com/oscarcpozas/gb_emu)",
-                WINDOW_WIDTH,
-                WINDOW_HEIGHT,
-                window_options
-            ) {
+            match Window::new(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, window_options) {
                 Ok(win) => win,
                 Err(err) => panic!("Error creating window {}", err)
             };
 
-        GUI {
-            window,
-            keys_states: GUI::new_key_states(),
-            vram: vec![0; WINDOW_WIDTH * WINDOW_HEIGHT]
-        }
-    }
 
-    pub fn run(mut self) {
-        while self.window.is_open() && !self.window.is_key_down(minifb::Key::Escape) {
-            std::thread::sleep(Duration::from_millis(20));
-            self.update_vram();
-            self.get_key_update();
+        while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
+            std::thread::sleep(Duration::from_millis(10));
+            self.update_vram(&mut window);
+            self.get_key_update(&window);
         }
     }
 
@@ -70,15 +65,13 @@ impl GUI {
         keys_states
     }
 
-    fn update_vram(&mut self) {
-        let vram = self.vram.clone();
-        self.window
-            .update_with_buffer(&vram, WINDOW_WIDTH, WINDOW_HEIGHT)
-            .unwrap();
+    fn update_vram(&mut self, window: &mut Window) {
+        let vram = self.vram.lock().unwrap().clone();
+        window.update_with_buffer(&vram, WINDOW_WIDTH, WINDOW_HEIGHT).unwrap();
     }
 
-    fn get_key_update(&mut self) {
-        for key in self.window.get_keys() {
+    fn get_key_update(&mut self, window: &Window) {
+        for key in window.get_keys() {
             let gb_key = match key {
                 minifb::Key::Right => GameBoyKey::Right,
                 minifb::Key::Left => GameBoyKey::Left,
@@ -94,7 +87,7 @@ impl GUI {
                 _ => continue,
             };
 
-            match self.keys_states.get_mut(&gb_key) {
+            match self.keys_states.lock().unwrap().get_mut(&gb_key) {
                 Some(value) => *value = true,
                 None => unreachable!(),
             }
