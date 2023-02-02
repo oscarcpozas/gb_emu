4262bb356af30e12a60a6e3684f25096404cdd2a
Oscar Caballero <oscarcpozas@gmail.com>
2023-02-02 17:19:37 +0100

    Sintax improvement on MMU

--- emu/src/mmu.rs
+++ emu/src/mmu.rs
@@ -3,36 +3,40 @@ pub struct Mmu {
 }
 
 impl Mmu {
-
-    // Echo ram sector contains the same data as C000-DDFF sector
-    fn is_echo_ram(addr: u16) -> bool {
-        addr >= 0x0000 && addr <= 0x1fff
+    pub fn new() -> Self {
+        Self {
+            ram: vec![0u8; 0x10000]
+        }
     }
 
     pub fn get8(&self, mut addr: u16) -> u8 {
-        if is_echo_ram(addr) {
-            addr -= 0x2000;
+        if self.is_echo_ram(addr) {
+            addr -= 0x2000; // Echo RAM sector, it's same content that C000-DDFF sector
         }
         self.ram[addr as usize]
     }
 
-    pub fn set8(&self, mut addr: u16, v: u8) {
-        // Echo RAM sector, it's same content that C000-DDFF sector
-        if is_echo_ram(addr) {
-            addr -= 0x2000;
+    pub fn set8(&mut self, mut addr: u16, v: u8) {
+        if self.is_echo_ram(addr) {
+            addr -= 0x2000; // Echo RAM sector, it's same content that C000-DDFF sector
         }
         self.ram[addr as usize] = v;
     }
 
     pub fn get16(&self, mut addr: u16) -> u16 {
-        let f = self.get8(addr) as u16;
-        let s = self.get8(addr + 1);
-        f << 8 | s
+        let l = self.get8(addr) as u16;
+        let h = self.get8(addr + 1) as u16;
+        h << 8 | l
     }
 
     // TODO: Review that part, I guess the memory persists inverted
-    pub fn set16(&self, mut addr: u16, v: u16) {
+    pub fn set16(&mut self, mut addr: u16, v: u16) {
         self.set8(addr, v as u8);
         self.set8(addr + 1, (v >> 8) as u8);
+    }
+
+    // Echo ram sector contains the same data as C000-DDFF sector
+    fn is_echo_ram(&self, addr: u16) -> bool {
+        addr >= 0xe000 && addr <= 0x1fff
     }
 }
