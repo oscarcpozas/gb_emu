5d76cfe8b7532efefbe49e5512fe5867aac70319
Oscar Caballero <oscarcpozas@gmail.com>
2023-03-11 16:26:51 +0100

    Added memory handlers to mmu

--- emu/src/mmu.rs
+++ emu/src/mmu.rs
@@ -1,15 +1,67 @@
+use std::cell::RefCell;
+use std::cmp::min;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+/// The variants to control memory read access from the CPU.
+pub enum MemRead {
+    /// Replaces the value passed from the memory to the CPU.
+    Replace(u8),
+    /// Shows the actual value passed from the memory to the CPU.
+    PassThrough,
+}
+
+/// The variants to control memory write access from the CPU.
+pub enum MemWrite {
+    /// Replaces the value to be written by the CPU to the memory.
+    Replace(u8),
+    /// Allows to write the original value from the CPU to the memory.
+    PassThrough,
+    /// Discard the write access from the CPU.
+    Block,
+}
+
+pub trait MemHandler {
+    /// The function is called when the CPU attempts to read from the memory.
+    fn on_read(&self, addr: u16) -> MemRead;
+
+    /// The function is called when the CPU attempts to write to the memory.
+    fn on_write(&mut self, addr: u16, value: u8) -> MemWrite;
+}
+
 pub struct Mmu {
-    ram: Vec<u8>
+    ram: Vec<u8>,
+    handlers: HashMap<u16, Vec<Rc<RefCell<dyn MemHandler>>>>,
 }
 
 impl Mmu {
     pub fn new() -> Self {
         Self {
-            ram: vec![0u8; 0x10000]
+            ram: vec![0u8; 0x10000],
+            handlers: HashMap::new(),
+        }
+    }
+
+    pub fn add_handler<T: MemHandler>(&mut self, range: (u16, u16), handler: Rc<RefCell<T>>) {
+        for i in range.0..=range.1 {
+            if self.handlers.contains_key(&i) {
+                self.handlers.get_mut(&i).unwrap().push(handler);
+            } else {
+                self.handlers.insert(i, vec![handler]);
+            }
         }
     }
 
     pub fn get8(&self, mut addr: u16) -> u8 {
+        if let Some(handlers) = self.handlers.get(&addr) {
+            for handler in handlers {
+                match handler.on_read(addr) {
+                    MemRead::Replace(value) => return value,
+                    MemRead::PassThrough => {}
+                }
+            }
+        }
+
         if self.is_echo_ram(addr) {
             addr -= 0x2000; // Echo RAM sector, it's same content that C000-DDFF sector
         }
